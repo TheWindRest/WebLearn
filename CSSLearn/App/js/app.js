cdc475b94fe4371606a15def701d540cdca00684
WindAndMan <1325390776@qq.com>
2020-05-08 16:22:30 +0800

    上传数据

---
+++ CSSLearn/App/js/app.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        projects: [
+            {
+                name: "1.BorderButton",
+                video: "https://www.bilibili.com/bangumi/play/ep317925?spm_id_from=333.851.b_62696c695f7265706f72745f616e696d65.52"
+            },
+            {
+                name: "2.Card",
+                video: "https://www.bilibili.com/bangumi/play/ep317925?spm_id_from=333.851.b_62696c695f7265706f72745f616e696d65.52"
+            },
+            {
+                name: "3.Email",
+                video: "https://www.bilibili.com/bangumi/play/ep317925?spm_id_from=333.851.b_62696c695f7265706f72745f616e696d65.52"
+            },
+            {
+                name: "4.Clock",
+                video: "https://www.bilibili.com/bangumi/play/ep317925?spm_id_from=333.851.b_62696c695f7265706f72745f616e696d65.52"
+            },
+        ]
+    },
+    methods: {
+        getImagePath(pathName) {
+            
+        }
+    }
+})
