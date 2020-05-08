1ed163ecfb27efcd4374a5efa1ec5b78e3d19fd3
WindAndMan <1325390776@qq.com>
2020-05-08 17:19:51 +0800

    带我去

---
+++ CSSLearn/App/js/hm.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?05a2292a20feb9a025e938bb43a25810";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
